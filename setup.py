00a6cec49742d8664b399038107370be7401afa8
Joel Hancock <joel@menchelab.com>
2022-05-04 15:53:44 +0200

    added setup for pip installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='utils',
+      version='0.0',
+      description="Joel's utilities",
+      author='Oscar the Grouch',
+      author_email='none@none.com',
+      packages=['utils']
+     )
