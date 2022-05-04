6c69caceea9e0ab63d5184e62e25ff21b0114293
Joel Hancock <joel@menchelab.com>
2022-05-04 17:12:21 +0200

    deleted annoying duplicates

--- umap_network.py
+++
@@ -1,6 +0,0 @@
-import umap
-import networkx as nx
-
-rndstate = np.random.RandomState(108)
-A,_,_ = umap.umap_.fuzzy_simplicial_set(features, 10, rndstate, "euclidean")
-G = nx.from_scipy_sparse_matrix(A)
