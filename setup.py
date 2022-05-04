596add673c1a4199d4153eb8c4c05907b5d7f17f
Joel Hancock <joel@menchelab.com>
2022-05-04 16:02:41 +0200

    py_modules, not packages

--- setup.py
+++ setup.py
@@ -5,7 +5,5 @@ from distutils.core import setup
 setup(name='utils',
       version='0.1',
       description="Joel's utilities",
-      author='Joel',
-      author_email='none@none.com',
-      packages=['utils']
+      py_modules=['utils']
      )
